69561f79bc3d0d50ecb5b819c9ae29159b6c7f69
chris <867746469@qq.com>
2020-06-24 15:36:36 +0800

    init

---
+++ barrageData.js
@@ -0,0 +1,32 @@
+/**
+ value : 内容，必填
+ time : 时间（s），必填
+ speed : 速度
+ color : 颜色，
+ fontSize: 字号
+ */
+const data = [{
+	value: '写代码的都好帅啊！！！',
+	speed: 2,
+	time: 1,
+	color: 'red',
+	fontSize: 20
+},{
+	value: '老铁，666啊！',
+	speed: 2,
+	time: 2,
+	color: 'orange',
+	fontSize: 24
+},{
+	value: '66666666666',
+	speed: 3,
+	time: 3,
+	color: 'pink',
+	fontSize: 28
+},{
+	value: '前端好难啊，求教学',
+	speed: 1,
+	time: 4,
+	color: 'blue',
+	fontSize: 30
+}]
